f2f88597bc036b7aeb83f6aca046edfce9df26c7
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-08-18 18:22:41 +0200

    second commit

---
+++ DynamicTable/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// eslint-disable-next-line
+import React, { useState } from 'react';
+import DynamicTable from './DynamicTable';
+
+function App() {
+  {
+    return (
+      <DynamicTable />
+    );
+  }
+}
+export default App;
