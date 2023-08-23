d956c87c6ca719577c7d80cffff6dab982057b92
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-08-23 02:24:38 +0200

    fifth commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // eslint-disable-next-line
 import React from 'react';
 import DynamicTable from './DynamicTable';
-//import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-import 'firebase/compat/database';
 
 function App() {
   return (
